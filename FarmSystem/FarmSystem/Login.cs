15436a985288ebf0bced2cdba200a35af8e9492c
Brandon Neal <398019@edu.cwa.ac.uk>
2018-11-05 09:29:23 +0000

    Setting up basic form design for login and manager/labourer forms.
    Added Logout and Exit buttons to each form, awaiting database to finalise login form.

---
+++ FarmSystem/FarmSystem/Login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FarmSystem
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ManagerForm mg = new ManagerForm();
+            this.Hide();
+            mg.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
