b53bd2e705a91b0534110a4b0d66ac54f5e81fed
Almaz Miriam Natalie Ann Wright <418906@edu.cwa.ac.uk>
2018-11-05 09:53:50 +0000

    Employee and vehicle changes

---
+++ FarmSystem/FarmSystem/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FarmSystem
+{
+    public class Employee
+    {
+        private string fName;
+        private string lName;
+        private string role;
+
+        public string FName { get { return fName; } }
+        public string LName { get { return lName; } }
+        public string Role { get { return role; } }
+
+        public class Manager : Employee
+        {
+
+        }
+
+        public class Labourer : Employee
+        {
+
+        }
+    }
+
+}
