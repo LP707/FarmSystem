cfc22cbe6f4fa691735aaf869bdc70b63e859028
Almaz Miriam Natalie Ann Wright <418906@edu.cwa.ac.uk>
2018-11-06 09:59:18 +0000

    Vehicle class updated

--- FarmSystem/FarmSystem/Vehicle.cs
+++ FarmSystem/FarmSystem/Vehicle.cs
@@ -8,12 +8,29 @@ namespace FarmSystem
 {
     class Vehicle
     {
+        private string wHeels;
+        private string dRiver;
+        private string aTach;
+
+        public string wheels { get { return wHeels; } }
+        public string driver { get { return dRiver; } }
+        public string atach { get { return aTach; } }
+
+        public class Tractor : Vehicle
+        {
+
+        }
+
+        public class Cmbhrv : Vehicle
+        {
+
+        }
 
 
+        public class Bailer : Vehicle
+        {
 
-
-
-
+        }
 
 
 
