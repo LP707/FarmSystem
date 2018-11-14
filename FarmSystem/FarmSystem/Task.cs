b3729d84158b47f325fc6e441350c9cdc01460a9
Almaz Miriam Natalie Ann Wright <418906@edu.cwa.ac.uk>
2018-11-14 09:30:36 +0000

    Task class added

---
+++ FarmSystem/FarmSystem/Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmSystem
+{
+ 
+        public class Task
+        {
+            private int taskID;
+            private string tName;
+            private string employeeT;
+            private string desc;
+
+            public int taskiD { get { return taskID; } set { taskID = value; } }
+            public string tnAme { get { return tName; } set { tName = value; } }
+            public string emplyT { get { return employeeT; } set { employeeT = value; } }
+            public string Desc { get { return desc; } set { desc = value; } }
+
+            public class Sewing : Task
+            {
+
+            }
+
+            public class Harvesting : Task
+            {
+
+            }
+
+             public class Treating : Task
+             {
+
+             }
+
+
+    }
+
+    }
+
+
+
